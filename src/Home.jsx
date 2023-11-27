3758427f8003ecdfcdcbdefe5b908b1675edabb1
AsGhrK <davicasari1@gmail.com>
2023-11-27 10:15:38 -0400

    Nav Bar
    Foi criado uma  pasta chamada "Componentes" para abrigar os componentes do site e agilizar o processo de criação  do site

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,8 @@
+import NavBar from "./Componentes/NavBar";
+
 export default function Home(){
     return(
-        <h1>PTAC 2 - PROJETO FINAL</h1>
+        <NavBar/>
      );
 }
 
-k
--- src/main.jsx
+++ src/main.jsx
@@ -10,5 +10,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <Route path="/" element={<Home />}></Route>
       </Routes>
     </BrowserRouter>
-  </React.StrictMode>,
+  </React.StrictMode>
 )
