efcceba908c3d6e03f2ac429e85895fcbc4cf228
AsGhrK <davicasari1@gmail.com>
2023-11-28 11:57:06 -0400

    Cadastrar 2 (incompleto)
    Foi criado um local para inserção dos dados do cadastro de vídeos, e também foi realizada a estilização utilizando CSS. No entanto, ainda não está totalmente pronto, pois há problemas na formatação. As funções, no entanto, estão operando corretamente.

--- src/Cadastrar/Cadastrar.jsx
+++ src/Cadastrar/Cadastrar.jsx
@@ -1,71 +1,142 @@
-import { useEffect, useState } from "react";
+// Cadastrar.jsx
+
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import NavBar from "../Componentes/NavBar";
-import "./Cadastrar.css"
+import "./Cadastrar.css";
 
 export default function Cadastrar() {
-    const listaLocalStorege = JSON.parse(localStorage.getItem("Lista")) || [];
-    const [produto, setProduto] = useState("");
-    const [preco, setPreco] = useState("");
-    const [lista, setLista] = useState(listaLocalStorege);
-    const [id, setId] = useState(listaLocalStorege[listaLocalStorege.length - 1]?.id + 1 || 1);
-  
-    useEffect(() => {
-      localStorage.setItem("Lista", JSON.stringify(lista));
-    }, [lista]);
-  
-    const salvar = (e) => {
-      e.preventDefault();
-      setLista([...lista, { produto: produto, preco: preco, id: id }]);
-      setId(id + 1);
-    };
-  
-    const removeProduto = (id) => {
-      const novaLista = lista.filter((prod) => prod.id !== id);
-      setLista(novaLista);
-    };
-  
-    return (
-    <div>
-        <NavBar/>
-      <div className="container">
-        <h1 style={{ color: "#007bff" }}>Lista de Produtos</h1>
-        <form onSubmit={salvar}>
-          <h2>
-            Nome:{" "}
-            <input value={produto} onChange={(e) => setProduto(e.target.value)} />
-          </h2>
-          <h2>
-            Preço:{" "}
+  const localStorageList = JSON.parse(localStorage.getItem("Lista")) || [];
+  const [videos, setVideo] = useState("");
+  const [url, setUrl] = useState("");
+  const [artista, setArtista] = useState("");
+  const [discricao, setDescricao] = useState("");
+  const [lista, setLista] = useState(localStorageList);
+  const [id, setId] = useState(
+    localStorageList[localStorageList.length - 1]?.id + 1 || 1
+  );
+
+  useEffect(() => {
+    localStorage.setItem("Lista", JSON.stringify(lista));
+  }, [lista]);
+
+  const salvar = (e) => {
+    e.preventDefault();
+    setLista([
+      ...lista,
+      { videos, url, artista, id, discricao },
+    ]);
+    setId(id + 1);
+  };
+
+  const removeVideo = (id) => {
+    const newLista = lista.filter((vid) => vid.id !== id);
+    setLista(newLista);
+  };
+
+  const [showFullDescription, setShowFullDescription] = useState({});
+
+  const toggleDescription = (id) => {
+    setShowFullDescription({
+      ...showFullDescription,
+      [id]: !showFullDescription[id],
+    });
+  };
+
+  return (
+    <div className="cadastrar-container">
+      <NavBar />
+      <div className="content-container">
+        <h1 className="title">Lista de Produtos</h1>
+        <form onSubmit={salvar} className="form">
+          <div className="form-group">
+            <label htmlFor="videos">Nome do Video:</label>
             <input
-              type="number"
-              value={preco}
-              onChange={(e) => setPreco(e.target.value)}
+              type="text"
+              id="videos"
+              value={videos}
+              onChange={(e) => setVideo(e.target.value)}
             />
-          </h2>
-          <button style={{ backgroundColor: "#007bff", color: "#fff" }}>
-            ADD
-          </button>
+          </div>
+          <div className="form-group">
+            <label htmlFor="url">URL:</label>
+            <input
+              type="text"
+              id="url"
+              value={url}
+              onChange={(e) => setUrl(e.target.value)}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="artista">Artista:</label>
+            <input
+              type="text"
+              id="artista"
+              value={artista}
+              onChange={(e) => setArtista(e.target.value)}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="discricao">Descrição:</label>
+            <input
+              type="text"
+              id="discricao"
+              value={discricao}
+              onChange={(e) => setDescricao(e.target.value)}
+            />
+          </div>
+          <br/>
+          <button className="add-button">ADD</button>
         </form>
-        {lista.map((prod) => (
-          <div className="produtos" key={prod.id}>
+      </div>
+      <div className="videos-container">
+        {lista.map((vid) => (
+          <div className="video-item" key={vid.id}>
             <p>
-              Nome: {prod.produto}, Preço: {prod.preco}, Id: {prod.id}
+              Nome: {vid.videos}
+              <br/>
+              Artista: {vid.artista}
+              <br/>
+              Descrição: {showFullDescription[vid.id]
+                ? vid.discricao
+                : `${vid.discricao.slice(0, 50)}...`}<br/>
+              {!showFullDescription[vid.id] ? (
+                <button
+                  className="ver-mais-button"
+                  onClick={() => toggleDescription(vid.id)}
+                >
+                  Ver Mais
+                </button>
+              ) : (
+                <button
+                  className="ver-mais-button"
+                  onClick={() => toggleDescription(vid.id)}
+                >
+                  Esconder
+                </button>
+              )}
             </p>
-            <Link className="link" to={`/detalhe/${prod.id}`}><button className="button2">Ver</button></Link>
-  
+            <iframe
+              width="300"
+              height="200"
+              src={`https://www.youtube.com/embed/${vid.url.slice(17)}`}
+              title="Video"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allowFullScreen
+            ></iframe>
+            <Link className="link" to={`/detalhe/${vid.id}`}>
+              <button className="view-button">Ver</button>
+            </Link>
             <button
-              onClick={() => removeProduto(prod.id)}
-              style={{ backgroundColor: "#d9534f", color: "#fff" }}
+              onClick={() => removeVideo(vid.id)}
+              className="remove-button"
             >
               Remover
             </button>
-  
-            
-  
           </div>
         ))}
       </div>
-      </div>
-    );
-  }
+    </div>
+  );
+}
