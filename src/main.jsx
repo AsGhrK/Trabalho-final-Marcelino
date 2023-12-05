2b644e28975984d6b4f9cc40ae2c6a2734e361ea
AsGhrK <davicasari1@gmail.com>
2023-12-05 11:13:32 -0400

    Rotas inacabado
    Há um problema nas rotas quando estou na página de detalhes de um vídeo e tento acessar 'Subir vídeo' ou 'Em Alta'. Atualmente, sou redirecionado para rotas incorretas, como detalhe/Cadastrar e detalhe/EmAlta. Precisamos corrigir esse comportamento para garantir a navegação adequada entre as páginas.

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './Home/Home';
-import Cadastrar from './Cadastrar/Cadastrar';
-import EmAlta from "./EmAlta/EmAlta";
-import Detalhe from './Detalhe/Detalhe';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './Home/Home'
+import Cadastrar from './Cadastrar/Cadastrar'
+import EmAlta from "./EmAlta/EmAlta"
+import Detalhe from './Detalhe/Detalhe'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="Cadastrar" element={<Cadastrar />} />
-        <Route path="EmAlta" element={<EmAlta />} />
-        <Route path="Detalhe/:id" element={<Detalhe />} />
+        <Route path="/" element={<Home />}></Route>
+        <Route path="Cadastrar" element={<Cadastrar />}></Route>
+        <Route path="EmAlta" element={<EmAlta />}></Route>
+        <Route path="/Detalhe/:id" element={<Detalhe />}></Route>
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
-);
+)
